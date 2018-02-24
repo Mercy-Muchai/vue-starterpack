aa3b440e27f9e856e1a041cfcf0b11985de63d94
Mercy-Muchai <mercy.muchai@andela.com>
2018-02-24 15:47:10 +0300

    pug will be used later add new component and routing Signed-off-by: Mercy-Muchai <mercy.muchai@andela.com>

---
+++ src/components/Subscribe.vue
@@ -0,0 +1,14 @@
+<template>
+<input type="text" v-model="Subscribe"/>
+</template>
+
+<script>
+export default {
+  name: 'Subscribe',
+  data () {
+    return {
+      Subscribe: 'New subscription'
+    }
+  }
+}
+</script>
